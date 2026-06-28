64d2992c34bf0c1243032a02d1c09de2efb31858
Vikas Pathak <65367880+Vikas-Pathak-123@users.noreply.github.com>
2026-06-28 10:44:55 +0530

    VIB-6: Add types barrel export

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export for all VibeChat types.
+ * Import from here instead of individual type files:
+ * e.g. import { User, Chat, Message } from "../types";
+ */
+export type { User } from "./user.types";
+export type { Chat } from "./chat.types";
+export type { Message } from "./message.types";
